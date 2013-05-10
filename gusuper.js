c6957da27b62286204f08ae5c67654fb30fa4fb9
Terry Brady <terrywbrady@gmail.com>
2013-05-10 11:28:03 -0400

    coder module code review comments
    Writespace, jQuery inclusion, rename include to .inc

--- gusuper.js
+++ gusuper.js
@@ -1,36 +1,44 @@
-//if (Drupal.jsEnabled){
-	jQuery(document).ready(function(){
-		jQuery("#search-form").parent().find("ol.search-results").replaceWith("<div class='box'/>");
-		jQuery("#search-form").parent().find("div.box").load("/gusuper/combotemplate", null, cbSearch);
-	});	
-//}
+/**
+ * @file
+ * Triggers individual searches via ajax.  If any component search is unavailable, other searches will still return values.
+ */
 
-var cbSearch = function(responseText, textStatus, xhr) {
-	jQuery("dl.search-results").append(jQuery(responseText));
-	var query = escape(jQuery("#edit-keys").val());
-	if (jQuery("#gusearch-libsite").is("*")) {
-		jQuery("#gusearch-libsite").load("/gusuper/libsite/" + query, null, cb);			
+//See http://drupal.org/node/1446420 for an explanation of this construct
+//See http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html for more explanation of the wrapper
+
+(function ($) {
+  $(document).ready(function(){
+	$("#search-form").parent().find("ol.search-results").replaceWith("<div class='box'/>");
+	$("#search-form").parent().find("div.box").load("/gusuper/combotemplate", null, cbSearch);
+  });	
+
+  var cbSearch = function(responseText, textStatus, xhr) {
+	$("dl.search-results").append($(responseText));
+	var query = escape($("#edit-keys").val());
+	if ($("#gusearch-libsite").is("*")) {
+		$("#gusearch-libsite").load("/gusuper/libsite/" + query, null, cb);			
 	}
-	if (jQuery("#gusearch-libguideapi").is("*")) {
-		jQuery("#gusearch-libguideapi").load("/gusuper/libguideapi/" + query, null, cb);			
+	if ($("#gusearch-libguideapi").is("*")) {
+		$("#gusearch-libguideapi").load("/gusuper/libguideapi/" + query, null, cb);			
 	}
-	if (jQuery("#gusearch-dspacehtml").is("*")) {
-		jQuery("#gusearch-dspacehtml").load("/gusuper/dspacehtml/" + query, null, cb);
+	if ($("#gusearch-dspacehtml").is("*")) {
+		$("#gusearch-dspacehtml").load("/gusuper/dspacehtml/" + query, null, cb);
 	}
-	if (jQuery("#gusearch-dspacesolr").is("*")) {
-		jQuery("#gusearch-dspacesolr").load("/gusuper/dspacesolr/" + query, null, cb);
+	if ($("#gusearch-dspacesolr").is("*")) {
+		$("#gusearch-dspacesolr").load("/gusuper/dspacesolr/" + query, null, cb);
 	}
-	if (jQuery("#gusearch-libanswer").is("*")) {
-		jQuery("#gusearch-libanswer").load("/gusuper/libanswer/" + query, null, cb);
+	if ($("#gusearch-libanswer").is("*")) {
+		$("#gusearch-libanswer").load("/gusuper/libanswer/" + query, null, cb);
 	}
-}
+  }
 
 
-var cb = function(responseText, textStatus, xhr) {
+  var cb = function(responseText, textStatus, xhr) {
 	if (textStatus == "error") {
-		jQuery(this).html("<div class='combo-summary'>Search request cannot be submitted at this time.</div>");
+		$(this).html("<div class='combo-summary'>Search request cannot be submitted at this time.</div>");
 	}
 	if (responseText == "") {
-		jQuery(this).html("<div class='combo-summary'>Search request cannot be submitted at this time.</div>");
+		$(this).html("<div class='combo-summary'>Search request cannot be submitted at this time.</div>");
 	}
-}
+  }
+}(jQuery));
